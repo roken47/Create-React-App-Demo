a431e65189fecefd036ba6f891e89feee3111f64
roken47 <mradeboer7@gmail.com>
2021-01-21 18:44:27 -0600

    demo completed

--- src/App.js
+++ src/App.js
@@ -15,16 +15,12 @@ import React, { Component } from 'react';
             <img src={logo} className="App-logo" alt="logo" />
             {/* Adding the Welcome component and passing the value "Eric" to the "name" prop */}
             <Welcome name="Zaddy" />
-            {/* Added the clock component here.Clock doesn't require any props. It manages everything with it's internal state */}
+            {/* Added the clock component here. Clock doesn't require any props. It manages everything with it's internal state */}
           <Clock />
           {/* Added the Contact component here. */}
           <Contact />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
         </div>
+      </div>
       );
     }
   }
