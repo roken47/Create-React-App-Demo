c7dd6b7b6cdfb1d3eff779c1d869dd543913228c
roken47 <mradeboer7@gmail.com>
2021-02-03 19:11:36 -0600

    submitted with wishlist notes in 404

--- src/components/404page/404Page.js
+++ src/components/404page/404Page.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { browserHistory } from 'react-router-dom';
 
+// Would like a text statement of "this path doesn't match any routes"
+// A setTimeout behavior to automatically redirect to home or last visited page
+// Hide navigation component
 function NoMatch({history}) {
     return (
         <div className="404-error">
             <h1>404 Page</h1>
+            
             <p>Redirecting to <span onClick={() => history.push('/')}>Welcome Component</span></p>
         </div>
     )
